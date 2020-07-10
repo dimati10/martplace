d7be357f377e88beac8f95336d6cf3527b4addc0
dimati10 <dmitrievd321@gmail.com>
2020-07-11 00:10:10 +0300

    add slider section

---
+++ app/js/main.js
@@ -0,0 +1,17 @@
+$(function () {
+
+    $(".rate-star").rateYo({
+      rating: 4.5,
+      ratedFill: "#ffc000",
+      starWidth: "15px",
+      readOnly: true
+    });
+  
+    $('.slider__inner').slick({
+      prevArrow: '<button class="slick-arrow slick-prev"></button>',
+      nextArrow: '<button class="slick-arrow slick-next"></button>',
+    });
+  
+    // var mixer = mixitup('.products__inner-box');
+  
+  });
