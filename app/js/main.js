8ebcf5a0e21bcbfe5a0c9a1950899933dd14cb09
dimati10 <dmitrievd321@gmail.com>
2020-07-12 23:46:36 +0300

    added products section

--- app/js/main.js
+++ app/js/main.js
@@ -6,12 +6,19 @@ $(function () {
       starWidth: "15px",
       readOnly: true
     });
+
+    $(".rate-products").rateYo({
+      rating: 4.5,
+      ratedFill: "#ffc000",
+      starWidth: "13px",
+      readOnly: true
+    });
   
     $('.slider__inner').slick({
       prevArrow: '<button class="slick-arrow slick-prev"></button>',
       nextArrow: '<button class="slick-arrow slick-next"></button>',
     });
   
-    // var mixer = mixitup('.products__inner-box');
+    var mixer = mixitup('.products__inner-box');
   
   });
