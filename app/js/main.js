1a3bf337785d446bc6040516f1d460e57ffbbb6f
dimati10 <dmitrievd321@gmail.com>
2020-07-13 21:14:03 +0300

    added followers-slider section

--- app/js/main.js
+++ app/js/main.js
@@ -4,6 +4,7 @@ $(function () {
       rating: 4.5,
       ratedFill: "#ffc000",
       starWidth: "15px",
+      spacing: "4px",
       readOnly: true
     });
 
@@ -11,10 +12,18 @@ $(function () {
       rating: 4.5,
       ratedFill: "#ffc000",
       starWidth: "13px",
+      spacing: "4px",
       readOnly: true
     });
   
     $('.slider__inner').slick({
+      prevArrow: '<button class="slick-arrow slick-prev"></button>',
+      nextArrow: '<button class="slick-arrow slick-next"></button>',
+    });
+
+    $('.followers-slider__inner').slick({
+      slidesToShow: 3,
+      slidesToScroll: 3,
       prevArrow: '<button class="slick-arrow slick-prev"></button>',
       nextArrow: '<button class="slick-arrow slick-next"></button>',
     });
