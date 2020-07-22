446151d9713d589112d958606168c58ba16ce84c
dimati10 <dmitrievd321@gmail.com>
2020-07-23 01:32:15 +0300

    added feedback-slider section

--- app/js/main.js
+++ app/js/main.js
@@ -18,14 +18,21 @@ $(function () {
   
     $('.slider__inner').slick({
       prevArrow: '<button class="slick-arrow slick-prev"></button>',
-      nextArrow: '<button class="slick-arrow slick-next"></button>',
+      nextArrow: '<button class="slick-arrow slick-next"></button>'
     });
 
     $('.followers-slider__inner').slick({
       slidesToShow: 3,
       slidesToScroll: 3,
       prevArrow: '<button class="slick-arrow slick-prev"></button>',
-      nextArrow: '<button class="slick-arrow slick-next"></button>',
+      nextArrow: '<button class="slick-arrow slick-next"></button>'
+    });
+
+    $('.feedback-slider__inner').slick({
+      slidesToShow: 2,
+      slidesToScroll: 2,
+      prevArrow: '<button class="slick-arrow slick-prev"></button>',
+      nextArrow: '<button class="slick-arrow slick-next"></button>'
     });
   
     var mixer = mixitup('.products__inner-box');
